af3811e52d7548041b8f091c25d52e5ff799dd88
PhaneeshB <b.phaneesh@gmail.com>
2023-08-03 20:28:11 +0530

    use dynamo on train

--- apps/language_models/scripts/llama_lora_finetune.py
+++ apps/language_models/scripts/llama_lora_finetune.py
@@ -1,9 +1,15 @@
 import transformers
+from transformers import Trainer
 import textwrap
 from transformers import LlamaTokenizer, LlamaForCausalLM
 import os
 import sys
-from typing import List
+from typing import Any, Dict, List, Optional, Union
+import torch_mlir
+from io import BytesIO
+from shark.shark_inference import SharkInference
+
+
 
 from peft import (
     LoraConfig,
@@ -24,6 +30,7 @@ import json
 
 # DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
+device = "cuda"
 
 df = pd.read_csv("bitcoin-sentiment-tweets.csv")
 # df.head()
@@ -165,7 +172,144 @@ data_collator = transformers.DataCollatorForSeq2Seq(
     tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
 )
 
-trainer = transformers.Trainer(
+def _remove_nones(fx_g: torch.fx.GraphModule) -> List[int]:
+    removed_indexes = []
+    for node in fx_g.graph.nodes:
+        if node.op == "output":
+            assert (
+                len(node.args) == 1
+            ), "Output node must have a single argument"
+            node_arg = node.args[0]
+            if isinstance(node_arg, (list, tuple)):
+                node_arg = list(node_arg)
+                node_args_len = len(node_arg)
+                for i in range(node_args_len):
+                    curr_index = node_args_len - (i + 1)
+                    if node_arg[curr_index] is None:
+                        removed_indexes.append(curr_index)
+                        node_arg.pop(curr_index)
+                node.args = (tuple(node_arg),)
+                break
+
+    if len(removed_indexes) > 0:
+        fx_g.graph.lint()
+        fx_g.graph.eliminate_dead_code()
+        fx_g.recompile()
+    removed_indexes.sort()
+    return removed_indexes
+
+def _unwrap_single_tuple_return(fx_g: torch.fx.GraphModule) -> bool:
+    """
+    Replace tuple with tuple element in functions that return one-element tuples.
+    Returns true if an unwrapping took place, and false otherwise.
+    """
+    unwrapped_tuple = False
+    for node in fx_g.graph.nodes:
+        if node.op == "output":
+            assert (
+                len(node.args) == 1
+            ), "Output node must have a single argument"
+            node_arg = node.args[0]
+            if isinstance(node_arg, tuple):
+                if len(node_arg) == 1:
+                    node.args = (node_arg[0],)
+                    unwrapped_tuple = True
+                    break
+
+    if unwrapped_tuple:
+        fx_g.graph.lint()
+        fx_g.recompile()
+    return unwrapped_tuple
+
+def _returns_nothing(fx_g: torch.fx.GraphModule) -> bool:
+    for node in fx_g.graph.nodes:
+        if node.op == "output":
+            assert (
+                len(node.args) == 1
+            ), "Output node must have a single argument"
+            node_arg = node.args[0]
+            if isinstance(node_arg, tuple):
+                return len(node_arg) == 0
+    return False
+
+def transform_fx(fx_g):
+    for node in fx_g.graph.nodes:
+        if node.op == "call_function":
+            if node.target in [
+                torch.ops.aten.empty,
+            ]:
+                # aten.empty should be filled with zeros.
+                if node.target in [torch.ops.aten.empty]:
+                    with fx_g.graph.inserting_after(node):
+                        new_node = fx_g.graph.call_function(
+                            torch.ops.aten.zero_,
+                            args=(node,),
+                        )
+                        node.append(new_node)
+                        node.replace_all_uses_with(new_node)
+                        new_node.args = (node,)
+
+    fx_g.graph.lint()
+
+
+def refbackend_torchdynamo_backend(
+    fx_graph: torch.fx.GraphModule, example_inputs: List[torch.Tensor]
+):
+    # handling usage of empty tensor without initializing
+    transform_fx(fx_graph)
+    fx_graph.recompile()
+    if _returns_nothing(fx_graph):
+        return fx_graph
+    removed_none_indexes = _remove_nones(fx_graph)
+    was_unwrapped = _unwrap_single_tuple_return(fx_graph)
+
+    mlir_module = torch_mlir.compile(
+        fx_graph, example_inputs, output_type="linalg-on-tensors"
+    )
+
+    bytecode_stream = BytesIO()
+    mlir_module.operation.write_bytecode(bytecode_stream)
+    bytecode = bytecode_stream.getvalue()
+
+    shark_module = SharkInference(
+        mlir_module=bytecode, device=device, mlir_dialect="tm_tensor"
+    )
+    shark_module.compile()
+
+    def compiled_callable(*inputs):
+        inputs = [x.numpy() for x in inputs]
+        result = shark_module("forward", inputs)
+        if was_unwrapped:
+            result = [
+                result,
+            ]
+        if not isinstance(result, list):
+            result = torch.from_numpy(result)
+        else:
+            result = tuple(torch.from_numpy(x) for x in result)
+            result = list(result)
+            for removed_index in removed_none_indexes:
+                result.insert(removed_index, None)
+            result = tuple(result)
+        return result
+
+    return compiled_callable
+
+
+class SharkTrainer(Trainer):
+
+    def train(self, resume_from_checkpoint: str | bool | None = None, trial: Any | Dict[str, Any] = None, ignore_keys_for_eval: List[str] | None = None, **kwargs):
+        
+        dyn_call = torch._dynamo.optimize(refbackend_torchdynamo_backend)(super().train)
+        return dyn_call(resume_from_checkpoint, trial, ignore_keys_for_eval, **kwargs)
+
+    # def _inner_training_loop(self, batch_size=None, args=None, resume_from_checkpoint=None, trial=None, ignore_keys_for_eval=None):
+    #     dyn_call = torch._dynamo.optimize(ref_backend)(super()._inner_training_loop)
+    #     return dyn_call(batch_size, args, resume_from_checkpoint, trial, ignore_keys_for_eval)
+
+
+
+trainer = SharkTrainer(
     model=model,
     train_dataset=train_data,
     eval_dataset=val_data,
@@ -183,5 +327,9 @@ model.state_dict = (
 model = torch.compile(model)
 
 print("[DEBUG] begin training")
+
+
 trainer.train()
+
+
 model.save_pretrained(OUTPUT_DIR)
